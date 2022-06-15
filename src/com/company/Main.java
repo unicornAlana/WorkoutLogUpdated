90ab31900b31c140d3dfe994aa2255727605d4f1
bass.alana <bass.alana@vashonsd.org>
2022-06-15 10:42:27 -0700

    User Input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
         WorkoutLog log = new WorkoutLog();
         Workout w = new Workout(3, 1.5, "Trampolining");
         Workout a = new Workout(5, "Soccer practice", true, 2, 8.5, false);
@@ -12,6 +15,24 @@ public class Main {
         log.printNumberedLog();
         w.setDayOfWeek(2);
         w.printWorkout(w);
-        System.out.println ("Workout suggestion: " + log.whatKind(true, 7, false, false, false, 2, true));
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Respond in true or false, are you injured?");
+        boolean injury = scan.nextBoolean();
+        System.out.println("What is the numbered day of the week? (Monday is 1)");
+        int day = scan.nextInt();
+        System.out.println("Are you in soccer season (true or false)?");
+        boolean soccer = scan.nextBoolean();
+        System.out.println("Are you in track season (true or false)?");
+        boolean track = scan.nextBoolean();
+        System.out.println("Did you already have practice today (true or false)?");
+        boolean practice = scan.nextBoolean();
+        System.out.println("How many hours do you have?");
+        double hours = scan.nextDouble();
+        System.out.println("Is it nice outside (true or false)?");
+        boolean weather = scan.nextBoolean();
+
+        System.out.println ("Workout suggestion: " + log.whatKind(injury, day, soccer, track, practice, hours, weather));
     }
 }
